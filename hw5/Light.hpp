6f0e90d38ca2519119aea30d5e5425dae20ae2d9
Github-Anderson <2455788720@qq.com>
2024-07-10 15:35:37 +0800

    Finish hw5

--- hw5/Light.hpp
+++ hw5/Light.hpp
@@ -2,14 +2,10 @@
 
 #include "Vector.hpp"
 
-class Light
-{
-public:
-    Light(const Vector3f& p, const Vector3f& i)
-        : position(p)
-        , intensity(i)
-    {}
-    virtual ~Light() = default;
-    Vector3f position;
-    Vector3f intensity;
+class Light {
+ public:
+  Light(const Vector3f& p, const Vector3f& i) : position(p), intensity(i) {}
+  virtual ~Light() = default;
+  Vector3f position;
+  Vector3f intensity;
 };
